16a3e753ef23c2ab44e2f267cc30175f5301ac16
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-12 19:31:53 -0300

    Arquivo de configuração do WebSocket

---
+++ src/main/java/com/chat/configuration/ConfigurationWebSocket.java
@@ -0,0 +1,10 @@
+package com.chat.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ConfigurationWebSocket {
+    
+}
