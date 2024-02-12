2c81c80937d93531a4d89500be33880cdbc68118
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-12 19:43:01 -0300

    Arquivo de configuração do WebSocket

--- src/main/java/com/chat/configuration/ConfigurationWebSocket.java
+++ src/main/java/com/chat/configuration/ConfigurationWebSocket.java
@@ -1,10 +1,30 @@
 package com.chat.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class ConfigurationWebSocket {
+public class ConfigurationWebSocket implements WebSocketMessageBrokerConfigurer{
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //endpoint para o usuário se conectar ao socket
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
     
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //registrar os endpoints
+        registry.addEndpoint("chat-socket")
+        .setAllowedOrigins("/https://localhost:4200")
+        .withSockJS();
+
+    }
+
 }
