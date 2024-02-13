359386942a3c22f9fed58e0ae6113f3387a1a64e
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-12 21:09:08 -0300

    Controller do WebSocket (em andamento)

---
+++ src/main/java/com/chat/controller/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.chat.controller;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import com.chat.Model.Message;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/chat/{roomId}")
+    @SendTo("/topic/{roomId}")
+    public Message chat(@DestinationVariable String roomId, Message message){
+        System.out.println("Teste:"+message);
+        return new Message(message.getMessage(), message.getUser());
+    }
+}
