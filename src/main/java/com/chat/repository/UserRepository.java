a77811b30ca4859de8ccbbb9f23efd0b8df46306
Yuri Kevin <yurikevin1405@gmail.com>
2024-02-15 18:57:24 -0300

    Repositório da classe User.

--- src/main/java/com/chat/repository/UserRepository.java
+++ src/main/java/com/chat/repository/UserRepository.java
@@ -1,13 +1,9 @@
 package com.chat.repository;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chat.Model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
 	User findByTelephone(Long telephone);
-	
-	
 }
